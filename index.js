6d07fe39ab70ff85b61a3806da04cf60894492bb
coderman2 <okquaye@st.ug.edu.gh>
2024-03-15 14:15:46 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql");
+const express = require("express");
+const bodyparser = require("body-parser");
+
+
+const app = express();
+
+app.listen(3000,() => {
+    console.log('Server configuration Succesfull...')
+});
+
+
